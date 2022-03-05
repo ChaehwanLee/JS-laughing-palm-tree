0cb2028e8d39ae33c8d928b2f5c53accb5606219
Chaehwan <oasisinyourmind@gmail.com>
2022-03-05 23:11:31 +0900

    Add ajax

--- Desktop/2022_iot/Work/ormproject/web/views.py
+++ Desktop/2022_iot/Work/ormproject/web/views.py
@@ -1,3 +1,7 @@
+import logging
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django_request_mapping import request_mapping
@@ -11,6 +15,38 @@ class MyView(View):
     @request_mapping("/", method="get")
     def home(self,request):
         return render(request,'home.html');
+
+    @request_mapping("/ajax", method="get")
+    def ajax(self, request):
+        context = {
+            'center': 'ajax.html'
+        };
+        return render(request, 'home.html', context);
+
+    @request_mapping("/ajaximpl", method="get")
+    def ajaximpl(self, request):
+        # [{},{},{}]
+        data = [];
+        for i in range(1, 10):
+            dic = {};
+            dic['id'] = 'id'+str(i);
+            dic['name'] = 'james' + str(i);
+            dic['age'] = i;
+            data.append(dic);
+        return HttpResponse(json.dumps(data),content_type='application/json');
+
+    @request_mapping("/iot", method="get")
+    def iot(self, request):
+        id = request.GET['id'];
+        temp = request.GET['temp'];
+        el = request.GET['el'];
+        # -------------------------------------------
+        el_logger = logging.getLogger('el_file');
+        el_logger.debug(id + ',' + temp + ',' + el);
+        # -------------------------------------------
+        return render(request, 'ok.html');
+
+
 
     @request_mapping("/login", method="get")
     def login(self, request):
@@ -27,8 +63,9 @@ class MyView(View):
         context = {};
         try:
             cust = Cust.objects.get(id=id); # 입력한 아이디가 db에 있느냐 없느냐
-            if cust.pwd == pwd: # 비밀번호가 같으면
-                request.session['sessionid'] = id; #정상
+            if cust.pwd == pwd:
+                request.session['sessionid'] = cust.id;
+                request.session['sessionname'] = cust.name;
                 context['center'] = 'loginok.html';
             else:
                 raise Exception;
@@ -66,4 +103,53 @@ class MyView(View):
             context['center'] = 'registerok.html';
             context['rname'] = name;
 
+        return render(request, 'home.html', context);
+
+    @request_mapping("/geo", method="get")
+    def geo(self, request):
+        context = {
+            'center': 'geo.html'
+        };
+        return render(request, 'home.html', context);
+
+# 새롭게 추가한 부분
+    @request_mapping("/geoimpl", method="get")
+    def geoimpl(self, request):
+        data = [];
+        dic1 = {};
+        dic1['content'] = '더베이 101'
+        dic1['lat'] = 35.156593
+        dic1['lng'] = 129.152030
+        dic1['target'] = 'http://www.thebay101.com/the-bay-101/enjoy/'
+        data.append(dic1);
+
+        dic2 = {};
+        dic2['content'] = 'The Westin Josun Busan'
+        dic2['lat'] = 35.155927
+        dic2['lng'] = 129.153801
+        dic2['target'] = 'https://josunhotel.com/m/hotel/westinBusan.do'
+        data.append(dic2);
+
+        dic3 = {};
+        dic3['content'] = 'Dongbaek Park'
+        dic3['lat'] = 35.153620
+        dic3['lng'] = 129.152213
+        dic3['target'] = 'https://kr.trip.com/travel-guide/attraction/busan/dongbaekseom-island-95269/'
+        data.append(dic3);
+
+        dic4 = {};
+        dic4['content'] = 'Nurimaru APEC House'
+        dic4['lat'] = 35.152330
+        dic4['lng'] = 129.151344
+        dic4['target'] = 'https://www.busan.go.kr/nurimaru'
+        data.append(dic4);
+
+        return HttpResponse(json.dumps(data), content_type='application/json');
+# 여기까지
+
+    @request_mapping("/geo2", method="get")
+    def geo2(self, request):
+        context = {
+            'center': 'geo2.html'
+        };
         return render(request, 'home.html', context);
